f1b3efaba52e5d2d4b23f33664199e9f86298f0e
nichoth <nichoth@nichoth.com>
2025-03-13 14:58:25 -0700

    style tweak

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -24,11 +24,33 @@ export const HomeRoute:FunctionComponent<{
 
     return html`<div class="route home">
         <h2>Add your contact info</h2>
+        <p>
+            This will create a pull request with the data you input below.
+            It will be reviewed by a human, and if it is not spam, it will
+            be merged.
+        </p>
+
+        <p>
+            The text box below accepts markdown.
+        </p>
+
+        <p>
+            This information <strong>will be visible publicly</strong>, on the internet.
+        </p>
+
+        <hr />
 
         <form onSubmit=${submit}>
             <${TextInput} name="username" displayName="Your name" />
             <${TextInput} type="email" name="email" displayName="email" />
-            <textarea name="text"></textarea>
+            <${TextInput}
+                type="text"
+                name="bluesky"
+                displayName="Bluesky handle, eg @nichoth.com"
+            />
+            <${TextInput} type="text" name="link" displayName="Any other link" />
+            <label for="text">Any other info (markdown is ok)</label>
+            <textarea id="text" name="text"></textarea>
             <div class="controls">
                 <button type="submit">
                     Submit
