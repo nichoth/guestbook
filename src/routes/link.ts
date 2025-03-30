62a5a6eb2c6a14bdead3b2fb7d91bcd26709bf9d
nichoth <nichoth@nichoth.com>
2025-03-29 21:28:00 -0700

    wip

--- src/routes/link.ts
+++ src/routes/link.ts
@@ -1,25 +1,41 @@
 import { html } from 'htm/preact'
 import { useCallback } from 'preact/hooks'
-import { State } from '../state.js'
 import { ELLIPSIS, NBSP } from '@substrate-system/util/constants'
 import { type FunctionComponent } from 'preact'
-import { Primary as BtnPrimary } from '../components/button-outline.js'
+import { clipboardCopy } from '@substrate-system/copy-button/copy'
+import { useComputed, useSignal } from '@preact/signals'
+import { register } from '@substrate-system/copy-button'
+import { Btn, Primary as BtnPrimary } from '../components/button-outline.js'
+import { State } from '../state.js'
 import './link.css'
+import '@substrate-system/copy-button/css'
 import Debug from '@substrate-system/debug'
-import { useSignal } from '@preact/signals'
 const debug = Debug()
+
+if (!window.customElements.get('copy-button')) {
+    register()
+}
 
 export const LinkRoute:FunctionComponent<{
     state:ReturnType<typeof State>
 }> = function ({ state }) {
     debug('link route', state)
     const roomName = useSignal<string|null>(null)
+    const roomUrl = useComputed(() => {
+        if (!roomName.value) return null
+        return `${location.href}/${roomName.value}`
+    })
 
     const initLink = useCallback(async (ev:MouseEvent) => {
         ev.preventDefault()
         debug('init')
         const code = await State.initAddDevice(state, 'hello websockets')
         roomName.value = code
+    }, [])
+
+    const copyUrl = useCallback((ev:MouseEvent) => {
+        ev.preventDefault()
+        clipboardCopy(roomUrl.value!)
     }, [])
 
     return html`<div class="route link">
@@ -40,15 +56,23 @@ export const LinkRoute:FunctionComponent<{
                     The new device should connect to this url:
                 </div>
                 <div class="url">
-                    <code>
-                        ${location.href}/${roomName.value}
-                    </code>
-                </div>` :
+                    <code>${roomUrl.value}</code>
+                    <span>
+                        <copy-button payload="${roomUrl.value}"></copy-button>
+                    </span>
+                </div>
+                ` :
                 null
             }
             
 
             <div class="controls">
+                ${roomName.value ?
+                    html`<${Btn} class="copy-btn" onClick=${copyUrl}>
+                        Copy URL
+                    <//>` :
+                    null
+                }
                 <${BtnPrimary} onClick=${initLink}>Add a device<//>
             </div>
         </div>
