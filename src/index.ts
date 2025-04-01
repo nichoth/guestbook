a4bc1fb4a8ed59a3b8e1b69c8d6788d715a16ea8
nichoth <nichoth@nichoth.com>
2025-04-01 11:55:53 -0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -77,7 +77,7 @@ export const Guestbook:FunctionComponent = function () {
         <header>
             <h1>
                 <a href="/">
-                    Bellingham Guestbook
+                    Guestbook
                 </a>
             </h1>
 
